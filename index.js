7189667d5165f65783c72de5876e1cc4cbdf35ac
Samuel Colvin <s@muelcolvin.com>
2020-01-03 16:47:16 +0000

    proxy working

--- index.js
+++ index.js
@@ -1,64 +1,91 @@
-addEventListener('fetch', event => {
-  event.respondWith(handle_request(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: {'content-type': 'text/plain'},
-  })
-}
+addEventListener('fetch', event => event.respondWith(handle_request(event)))
+
+
+const description = (
+  "https://proxy.samuelcolvin.workers.dev will proxy a request to the URL specified by the 'upstream' GET parameter. " +
+  'Request method, headers and body will be forwarded unchanged if possible. ' +
+  'This function will return immediate, but the proxied request will continue for as long as possible, this ' +
+  'is useful when you want to fire a webhook to an endpoint which may take a long time to startup and respond.'
+)
 
 async function handle_request(event) {
   const params = new URL(event.request.url).searchParams
   const upstream = params.get('upstream')
   if (!upstream) {
-    return new Response('no "upstream" argument found, not proxying', {
-      headers: {'content-type': 'text/plain'},
-      status: 400,
-    })
+    return response({status: 'error', summary: "no 'upstream' get parameter found"}, 400)
+  }
+  try {
+    new URL(upstream)
+  } catch (e) {
+    return response({status: 'error', summary: 'invalid url', details: e.toString()}, 400)
   }
 
-  await proxy(upstream, event.request)
+  const headers = get_headers(event.request)
+  const body = await get_body(event.request)
+  const method = event.request.method
+  event.waitUntil(fetch(upstream, {method, headers, body}))
+  // let error = null
+  // try {
+  //   await fetch(upstream, {method, headers, body})
+  // } catch (e) {
+  //   error = {string: e.toString(), stack: e.stack}
+  //   console.error('error:', e, e.stack)
+  // }
 
-  return new Response(`request proxied to "${upstream}"`, {
-    headers: {'content-type': 'text/plain'},
+  return response({
+    status: 'success',
+    upstream: {
+      url: upstream,
+      method,
+      headers,
+      body_length: body ? body.length : null,
+    }
   })
 }
 
-async function proxy(url, request) {
-  await fetch(url, {
-    method: request.method,
-    headers: _get_headers(request),
-    body: await request.blob(),
-  })
+function response(data, status = 200) {
+  const body = JSON.stringify(Object.assign({description}, data), null, 2) + '\n'
+  const init = {headers: {'content-type': 'application/json'}, status}
+  return new Response(body, init)
 }
 
 const ignored_headers = new Set([
-  'accept-encoding',
-  'cf-connecting-ip',
-  'cf-ew-via',
-  'cf-ipcountry',
-  'cf-ray',
-  'cf-visitor',
-  'connect-time',
-  'connection',
-  'content-length',
-  'host',
-  'total-route-time',
-  'user-agent',
-  'via',
-  'x-forwarded-for',
-  'x-forwarded-port',
-  'x-forwarded-proto',
-  'x-request-id',
-  'x-request-start',
-  'steam-donkey-authorization',
+  'accept-encoding', 'cf-connecting-ip', 'cf-ipcountry', 'cf-ray', 'cf-visitor', 'connection', 'host',
+  'x-forwarded-proto', 'x-real-ip'
 ])
 
-function _get_headers(request) {
-  const keys = Object.keys(request.headers).filter(k => !ignored_headers.has(k))
-  return Object.assign(...keys.map(k => ({[k]: request.headers[k]})))
+const get_headers = request => {
+  const h = Array.from(request.headers.entries()).filter(h => !ignored_headers.has(h[0]))
+  const ip = request.headers.get('x-real-ip')
+  h.push(['x-forwarded-for', ip])
+  h.push(['x-proxied-ip', ip])
+  h.push(['x-proxied-url', request.url])
+  return Object.assign(...h.map(([k, v]) => ({[k]: v})))
+}
+
+async function get_body (request) {
+  if (['GET', 'HEAD'].includes(request.method)) {
+    return null
+  }
+  const reader = request.body && request.body.getReader()
+  if (!reader) {
+    return null
+  }
+  const chunks = []
+
+  while (true) {
+    const {done, value} = await reader.read()
+    if (done) {
+      break
+    }
+    chunks.push(value)
+  }
+
+  const buffer_view = new Uint8Array(chunks.map(b => b.length).reduce((a, b) => a + b, 0))
+  let pos = 0
+  for (let b of chunks) {
+    buffer_view.set(new Uint8Array(b), pos)
+    pos += b.length
+  }
+  return buffer_view
 }
